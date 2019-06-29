959a8a6e67fbed1d29393b174a05c7d7717bb0a0
JMC TOR <juancard190@gmail.com>
2019-06-29 17:03:47 -0500

    Assignment 4: Task1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,7 +9,7 @@ import Footer from './FooterComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { actions } from 'react-redux-form';
-import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators'
+import { postComment, fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators'
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const mapStateToProps = state => {
@@ -25,6 +25,7 @@ const mapDispatchToProps = dispatch => ({
   
   fetchDishes: () => { dispatch(fetchDishes())},
   resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
+  fetchLeaders: () => dispatch(fetchLeaders()),
   postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
   fetchComments: () => dispatch(fetchComments()),
   fetchPromos: () => dispatch(fetchPromos())
@@ -42,6 +43,7 @@ class Main extends Component {
     this.props.fetchDishes();
     this.props.fetchComments();
     this.props.fetchPromos();
+    this.props.fetchLeaders()
   }
   
   
@@ -60,7 +62,10 @@ class Main extends Component {
         promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
         promoLoading={this.props.promotions.isLoading}
         promoErrMess={this.props.promotions.errMess}
-        leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+        leader={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
+        leadersLoading={this.props.leaders.isLoading}
+        leaderErrMess={this.props.leaders.errMess}
+
     />
       );
     }
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -42,6 +42,48 @@ export const postComment = (dishId, rating, author, comment) => (dispatch) => {
   .catch(error =>  { console.log('post comments', error.message); alert('Your comment could not be posted\nError: '+error.message); });
 };
 
+
+
+
+export const fetchLeaders = () => (dispatch) => {
+   
+    dispatch(leadersLoading(true));
+
+    return fetch(baseUrl + 'leaders')
+    .then(response=>{
+      if(response.ok){
+        return response;
+      }else{
+        var error  = new ErrorEvent('Error '+ response.status + ':'+ response.statusText);
+        error.response = response;
+        throw error;
+      }
+    },
+    error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+    })
+    .then(response => response.json())
+    .then(leaders =>dispatch(addLeaders(leaders)))
+    .catch(error=> dispatch(leadersFailed(error.message)))
+};
+export const addLeaders = (leaders)=>({
+  type: ActionTypes.ADD_LEADERS,
+  payload: leaders
+})
+
+export const leadersLoading = ()=>({
+  type: ActionTypes.LEADERS_LOADING
+});
+
+export const leadersFailed = (errmess) =>({
+  type: ActionTypes.LEADERS_FAILED,
+  payload: errmess
+});
+
+
+
+
 export const fetchDishes = () => (dispatch) => {
 
     dispatch(dishesLoading(true));
@@ -51,11 +93,8 @@ export const fetchDishes = () => (dispatch) => {
         if (response.ok) {
           return response;
         } else {
-          console.log("sda")
           var error = new Error('Error ' + response.status + ': ' + response.statusText);
-          
           error.response = response;
-          
           throw error;
         }
       },
